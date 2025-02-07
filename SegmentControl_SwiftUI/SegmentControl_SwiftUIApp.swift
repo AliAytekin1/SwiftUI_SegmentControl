dd09922753ccc92d7303c11330b79f0315c847db
Ali Aytekin <aliaytekin027@gmail.com>
2025-02-07 14:59:22 +0300

    Initial Commit

---
+++ SegmentControl_SwiftUI/SegmentControl_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentControl_SwiftUIApp.swift
+//  SegmentControl_SwiftUI
+//
+//  Created by Ali Aytekin on 7.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SegmentControl_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
